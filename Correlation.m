836fedb38f011d54a2f20e7502974d4a49b2721b
Ricardo G <rich030rich@gmail.com>
2021-07-26 21:56:39 -0500

    Corrección de los coeficientes a

--- Correlation.m
+++ Correlation.m
@@ -1,23 +1,17 @@
 classdef Correlation
   methods(Static)
-    
-    function acorrSamples = AutoCorrs(samples, elems) 
-      N = length(samples);
-      acorrSamples = zeros(N,elems);
-      parfor i = [1:N]
-        acorrSamples(i,:) = Correlation.ACorrelation(samples(i,:),elems);
-      end
-    end
    
     
-    %Obtiene los primeros N terminos de la autocorrelación de una señal
-    %(vector renglón)
+    %Calcula la autocorrelación para cada una las señales contenidas en la 
+    %matriz signal, cada renglón representa una señal, N representa el
+    %número de elementos requeridos de la correlación.
     function corr = ACorrelation (signal, N)
       sizeS = size(signal);
       corr = zeros(sizeS(1),N);
       for k = 1:sizeS(1)
         for i = 1:N
-          simetric = [signal(k,i:end) zeros(1,i-1)];
+          simetric = [signal(k,i:end) zeros(1,i-1)]; %Señal desplazada a la izquierda con cada iteración 
+          signal(k,:);
           corr(k,i) = dot(signal(k,:), simetric)/N;
         end
         
--- Wienner.m
+++ Wienner.m
@@ -6,16 +6,19 @@ classdef Wienner
 
   methods(Static)
     
+    %Calcula los coefientes a a partir de una matriz de correlación, cada
+    %renglón de la matriz representa la correlación de un bloque.
     function predictors = centPredictors(corr)
       n = size(corr);
       predictors = zeros(n(1),Wienner.Orden);
       for i = 1:n(1)
-        predictors(i,:) = Wienner.pedictor(corr(i,:))';
+        predictors(i,:) = ones([1,Wienner.Orden]) - Wienner.pedictor(corr(i,:))';
       end
     end
     
     
-    
+    %Calcula el predictor de wienner utilizando un vector de correlaciones,
+    %si el vector es de tamaño N devuelve un predictor de tamaño N-1
     function W = pedictor(rx)
       Rx = Wienner.matCorre(rx(1:end-1));
       Rx_inv = inv(Rx);
@@ -23,7 +26,7 @@ classdef Wienner
     end
     
     
-    
+    %Crea la matriz simétrica de correlaciones.
     function matriz = matCorre(rx)
       n = length(rx);
       matriz = zeros(Wienner.Orden,Wienner.Orden);
